e19abc79481f8c5365871959186a10d3867d3c4e
Ryuichi Matsuoka <r.matsuoka@protonmail.com>
2022-05-03 16:58:55 +0900

    example/u9fs: change a default listen addr as 0.0.0.0:5640 (analogus to the default 9p port 564) and a default root dir as home dir

--- example/u9fs/main.go
+++ example/u9fs/main.go
@@ -9,15 +9,24 @@ import (
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	homedir, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	var listener net.Listener
+	listener, err = net.Listen("tcp", "0.0.0.0:5640")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err)
+			continue
 		}
-		go ya9p.Serve(conn, ya9p.FS(os.DirFS("/")))
+		log.Printf("connected from %v", conn.RemoteAddr())
+		go ya9p.Serve(conn, ya9p.FS(os.DirFS(homedir)))
 	}
 }
